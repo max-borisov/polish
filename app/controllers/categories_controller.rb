5acd65ef1a6af4ed0eb6466d1a721e41261a7da6
max-borisov <max.borisov@yahoo.com>
2016-02-14 19:13:47 +0200

    Show category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,8 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.select(:id, :title).order(id: :asc).all
   end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 end
